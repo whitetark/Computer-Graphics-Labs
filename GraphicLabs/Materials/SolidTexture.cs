d9124a805a9130f18985681ae0f3bee02e172437
Andrey Miroshnichenko <andreymirosh02@gmail.com>
2022-06-28 16:23:23 +0300

    That was a moment that he knew..

--- GraphicLabs/Materials/SolidTexture.cs
+++ GraphicLabs/Materials/SolidTexture.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GraphicLabs.Basic;
 
 namespace GraphicLabs.Materials
 {
-    internal class SolidTexture
+    public class SolidTexture : ITexture
     {
+        private readonly Vector colorish;
+        public SolidTexture(Vector Color)
+        {
+            colorish = Color;
+        }
+
+        public Vector GetColor(float u, float v, Vector vec)
+        {
+            return colorish;
+        }
     }
 }
--- GraphicLabs/Materials/Specular.cs
+++ GraphicLabs/Materials/Specular.cs
@@ -13,14 +13,25 @@ namespace GraphicLabs.Materials
 
     public class Specular : IMaterial
     {
-        private Vector _specVector;
-        private static Vector blackVector=new Vector(0f, 0f, 0f);
-
-        public Specular(Vector specVector)
+        private readonly ITexture albedo;
+        private readonly float refraction;
+        public Specular(ITexture albedo, float refraction)
         {
-            _specVector = specVector;
+            this.albedo = albedo;
+            this.refraction = refraction;
         }
 
+        public Specular(float refraction, Vector albedo)
+        {
+            this.albedo = new SolidTexture(albedo);
+            this.refraction=refraction;
+        }
 
+        public bool Scatter(Ray ray, Hit hit, ref Vector vec, ref Ray scattered)
+        {
+            vec = albedo.GetColor(hit.u, hit.v, hit.vector);
+            var refracted = vec.X;
+            return true;
+        }
     }
 }
