6593fb19d228e2c848e7ba68ad9a447e3c83ea34
Andrey Miroshnichenko <andreymirosh02@gmail.com>
2022-06-28 16:27:57 +0300

    moment that he knew x3

--- GraphicLabs/Materials/ITexture.cs
+++ GraphicLabs/Materials/ITexture.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GraphicLabs.Basic;
 
 namespace GraphicLabs.Materials
 {
-    internal class ITexture
+    public interface ITexture
     {
-    }
+        Vector GetColor(float u, float v, Vector vec);
+    }   
 }
